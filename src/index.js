613ed650821f62dac95a377210c7637e066c27cb
Nikita Ivanchikov <niva.pho@gmail.com>
2020-10-01 21:56:49 +0300

    init: start  Revers Int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let res;
+    res = parseInt(n.toString().match(/[\d]/g).reverse().join(''));
+
+    // let res = 0;
+    // n = Math.abs(n);
+
+    // while(n) {
+    //     res = res*10 + n % 10;
+    //     n = Math.floor( n / 10 );
+    // }
+    
+    return res;
 }
